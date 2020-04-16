68e36e01be001e6e2994300db6989b33a2300f40
A-DEEB <amd.deeb@outlook.com>
2020-04-16 11:29:21 -0400

    added Firebase DocumentRef Storage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './components/sign-in/sign-in.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 
@@ -26,9 +26,10 @@ class App extends React.Component {
 
     componentDidMount(){
 
-       this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {this.setState({currentUser: user});
-         console.log(user);
-      })
+       this.unsubscribeFromAuth =  auth.onAuthStateChanged(async user => {
+         
+       createUserProfileDocument(user);
+      });
 
       }
 componentWillUnmount() {
